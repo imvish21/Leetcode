68959ef3751493317f7f6cb09b4382458dd8a79b
Vishal Kumar <imvgupta21@gmail.com>
2023-08-19 00:23:55 +0530

    sliding window 567

---
+++ 567. Permutation in String/solution.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool checkInclusion(string pat, string txt) {
+             unordered_map<char,int>mp;
+	    int k = pat.length();
+	    int size = txt.length();
+	    for(int i = 0; i<pat.length(); i++)
+	    {
+	        mp[pat[i]]++;
+	    }
+	    int count = mp.size();
+	    int i = 0;
+	    int j = 0;
+	    int ans=0;
+	    while(j<size)
+	    {
+	        if(mp.find(txt[j])!=mp.end())
+	        {
+	            mp[txt[j]]--;
+	            if(mp[txt[j]] == 0)
+	            {
+	                count--;
+	            }
+	        }
+	        if(j-i+1<k)
+	        {
+	            j++;
+	        }
+	        else if(j-i+1 == k)
+	        {
+	            if(count == 0)
+	            {
+	                ans++;
+	            }
+	            if(mp.find(txt[i])!=mp.end())
+	            {
+	                mp[txt[i]]++;
+	                if(mp[txt[i]]==1)
+	                {
+	                    count++;
+	                }
+	            }
+	            j++;
+	            i++;
+	        }
+	    }
+	    if(ans>0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
